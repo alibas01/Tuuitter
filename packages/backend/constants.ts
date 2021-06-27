8bab53f67d2c792275907e4bf7c6905d7e12219e
Ali Bas <alibas01@gmail.com>
2021-06-26 23:02:40 -0400

    first commit for api

--- packages/backend/constants.ts
+++
@@ -1,1 +0,0 @@
-export const mongoConnectionString = "mongodb://localhost/twitterbean";
--- packages/frontend/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "regenerator-runtime/runtime"; // required polyfill
-
-ReactDOM.render(<App />, document.getElementById("out"));
